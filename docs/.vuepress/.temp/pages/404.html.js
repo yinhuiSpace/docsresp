34a055eb11cb48ef2acb45666debd07128447444
yinhuiSpace <2705696192@qq.com>
2024-07-06 10:32:37 +0800

    refactor:重构目录结构

--- docs/.vuepress/.temp/pages/404.html.js
+++ docs/.vuepress/.temp/pages/404.html.js
@@ -1,4 +1,4 @@
-import comp from "C:/Users/root/Desktop/docsresp/vuepress-starter/docs/.vuepress/.temp/pages/404.html.vue"
+import comp from "C:/Users/root/Desktop/docsresp/docs/.vuepress/.temp/pages/404.html.vue"
 const data = JSON.parse("{\"path\":\"/404.html\",\"title\":\"\",\"lang\":\"en-US\",\"frontmatter\":{\"layout\":\"NotFound\"},\"headers\":[],\"git\":{},\"filePathRelative\":null}")
 export { comp, data }
 
